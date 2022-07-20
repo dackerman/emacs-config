15650b4e84f9130df85b30b7d44d1559043a3a48
David Ackerman <david.w.ackerman@gmail.com>
2022-07-19 20:23:22 -0700

    checking in packages for... safety

---
+++ packages/async-20210823.528/async-pkg.el
@@ -0,0 +1,12 @@
+(define-package "async" "20210823.528" "Asynchronous processing in Emacs"
+  '((emacs "24.4"))
+  :commit "fd7a9fca4a7bd0690e2e7e209397f493194e4f12" :authors
+  '(("John Wiegley" . "jwiegley@gmail.com"))
+  :maintainer
+  '("John Wiegley" . "jwiegley@gmail.com")
+  :keywords
+  '("async")
+  :url "https://github.com/jwiegley/emacs-async")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ packages/notmuch-20220126.1122/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20220126.1122" "run notmuch within emacs" 'nil :commit "0a32741fceb7778ced34064eacb7b5aac2c71638" :url "https://notmuchmail.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
