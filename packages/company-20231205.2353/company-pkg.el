4c27e626c282528159631dfe4ceb868a277ae84b
David Ackerman <david.w.ackerman@gmail.com>
2023-12-29 20:06:13 -0500

    Upgraded packages

---
+++ packages/company-20231205.2353/company-pkg.el
@@ -0,0 +1,14 @@
+(define-package "company" "20231205.2353" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "7bcffac8f8d20164629a1de4727e991d64fefd5f" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainers
+  '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainer
+  '("Dmitry Gutov" . "dmitry@gutov.dev")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- packages/notmuch-20220714.1551/notmuch-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "notmuch" "20220714.1551" "run notmuch within emacs" 'nil :commit "f599b8873f4fc748ae1befdac776b543e61045d7" :url "https://notmuchmail.org")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
