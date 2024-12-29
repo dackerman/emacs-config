2ed165ed8f01932f882e6706425c4ca2ddd7d8d4
David Ackerman <david.w.ackerman@gmail.com>
2024-12-29 13:22:21 -0500

    fixup! Upgrade packages

---
+++ packages/ellama-20241129.2147/ellama-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ellama" "20241129.2147"
+  "Tool for interacting with LLMs."
+  '((emacs     "28.1")
+    (llm       "0.6.0")
+    (spinner   "1.7.4")
+    (transient "0.7")
+    (compat    "29.1"))
+  :url "https://github.com/s-kostyaev/ellama"
+  :commit "d4927093e2988084eac86c3254a7b7f5d4cbd761"
+  :revdesc "d4927093e298"
+  :keywords '("help" "local" "tools")
+  :authors '(("Sergey Kostyaev" . "sskostyaev@gmail.com"))
+  :maintainers '(("Sergey Kostyaev" . "sskostyaev@gmail.com")))
